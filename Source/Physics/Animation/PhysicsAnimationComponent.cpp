362acd7f41451b26fb2f10822fa2b4d0a17d0c68
Eneko Etxaniz Monge <eenneekkooeemm@gmail.com>
2026-06-16 21:27:10 +0200

    End

--- Source/Physics/Animation/PhysicsAnimationComponent.cpp
+++ Source/Physics/Animation/PhysicsAnimationComponent.cpp
@@ -20,16 +20,88 @@ void UPhysicsAnimationComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	if (AActor* OwnerActor = GetOwner())
+	if (AActor* pOwnerActor = GetOwner())
 	{
-		m_Mesh = GetOwner()->GetComponentByClass<USkeletalMeshComponent>();
+		m_Mesh = pOwnerActor->GetComponentByClass<USkeletalMeshComponent>();
+		pOwnerActor->OnTakePointDamage.AddDynamic(this, &UPhysicsAnimationComponent::OkTakePointDamage);
+	}
+
+	m_fBlendTimer = 0.f;
+}
+
+void UPhysicsAnimationComponent::OkTakePointDamage(AActor* DamagedActor, float Damage, AController* InstigatedBy, FVector HitLocation, UPrimitiveComponent* FHitComponent, FName BoneName, FVector ShotFromDirection, const UDamageType* DamageType, AActor* DamageCauser)
+{
+	if (!m_Mesh || !m_bEnabled) return;
+
+
+	bool bBreakable = false;
+	
+	if (m_tBreakableBones.Contains(BoneName))
+	{
+		bBreakable = true;
+
+		// FVector Impulse, FVector HitLocation, FName InBoneName
+		m_Mesh->BreakConstraint(FVector::ZeroVector, HitLocation, BoneName);
+		m_Mesh->SetBodySimulatePhysics(m_sAnimatedBoneName, true);
+
+		// DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FConstraintBrokenSignature, int32, ConstraintIndex);
+		m_Mesh->OnConstraintBroken.Broadcast(m_Mesh->FindConstraintIndex(BoneName));
+
+		m_tBrokenBones.AddUnique(BoneName);
+	}
+	else
+	{
+		m_sAnimatedBoneName = m_sOverrideAnimatedBoneName.IsNone() ? BoneName : m_sOverrideAnimatedBoneName;
+		
+		m_Mesh->SetAllBodiesBelowSimulatePhysics(m_sAnimatedBoneName, true, true);
+
+		m_fBlendTimer = m_fBlendDuration;
 	}
 	
+	if (GEngine) GEngine->AddOnScreenDebugMessage(-1, 15.0f, bBreakable?FColor::Green:FColor::Red, BoneName.ToString());
+
+}
+
+void UPhysicsAnimationComponent::EndComponent()
+{
+	m_fBlendTimer = 0.f;
+	m_bEnabled = false;
+	m_Mesh->SetAllBodiesBelowPhysicsBlendWeight(m_sAnimatedBoneName, 1);
+	m_Mesh->SetAllBodiesBelowSimulatePhysics(m_sAnimatedBoneName, false, true);
+	m_tBrokenBones.Empty();
+}
+
+void UPhysicsAnimationComponent::CustomSetAllBodiesBelowPhysicsBlendWeight(const FName& InBoneName, float PhysicsBlendWeight, bool bSkipCustomPhysicsType, bool bIncludeSelf)
+{
+	m_Mesh->SetAllBodiesBelowPhysicsBlendWeight(InBoneName, PhysicsBlendWeight, bSkipCustomPhysicsType, bIncludeSelf);
+
+	int32 NumBodiesFound = m_Mesh->ForEachBodyBelow(InBoneName, bIncludeSelf, bSkipCustomPhysicsType, [PhysicsBlendWeight, this, InBoneName](FBodyInstance* BI)
+		{
+				BI->PhysicsBlendWeight = (m_tBrokenBones.Find(InBoneName)) ? 1 : PhysicsBlendWeight;
+		});
 }
 
 // Called every frame
 void UPhysicsAnimationComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	if (m_fBlendTimer > 0.f)
+	{
+		m_fBlendTimer = FMath::Max(m_fBlendTimer, 0.f);
+		m_fBlendTimer -= DeltaTime;
+
+		if (m_pCurve && m_Mesh)
+		{
+			//CustomSetAllBodiesBelowPhysicsBlendWeight(m_sAnimatedBoneName, m_pCurve->GetFloatValue(1.f - m_fBlendTimer/m_fBlendDuration));
+			m_Mesh->SetAllBodiesBelowPhysicsBlendWeight(m_sAnimatedBoneName, m_pCurve->GetFloatValue(1.f - m_fBlendTimer / m_fBlendDuration));
+		}
+
+		if (m_fBlendTimer <= 0.f)
+		{
+			bool bNewSimulate = false;
+			m_Mesh->SetAllBodiesBelowSimulatePhysics(m_sAnimatedBoneName, bNewSimulate, true);
+		}
+	}
 }
 
--- Source/Physics/Animation/PhysicsAnimationComponent.h
+++ Source/Physics/Animation/PhysicsAnimationComponent.h
@@ -10,17 +10,49 @@ class PHYSICS_API UPhysicsAnimationComponent : public UActorComponent
 {
 	GENERATED_BODY()
 
+	// ----------------------------- Variables
+public:	
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Config)
+	USkeletalMeshComponent* m_Mesh;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Config)
+	UCurveFloat* m_pCurve;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Config)
+	float m_fBlendDuration;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Config)
+	FName m_sOverrideAnimatedBoneName;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Config)
+	TArray<FName> m_tBreakableBones;
+
+
+protected:
+	float m_fBlendTimer = 0;
+	FName m_sAnimatedBoneName;
+	bool m_bEnabled = true;
+	TArray<FName> m_tBrokenBones;
+	// ----------------------------- Methods
+
 public:	
 	// Sets default values for this component's properties
 	UPhysicsAnimationComponent();
 
-protected:
-	// Called when the game starts
-	virtual void BeginPlay() override;
-public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite, Category = Config)
-	USkeletalMeshComponent* m_Mesh;
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+	UFUNCTION()
+	void OkTakePointDamage(AActor* DamagedActor, float Damage, class AController* InstigatedBy, FVector HitLocation, class UPrimitiveComponent* FHitComponent, FName BoneName, FVector ShotFromDirection, const class UDamageType* DamageType, AActor* DamageCauser);
+
+	UFUNCTION(BlueprintCallable)
+	void EndComponent();
+
+	void CustomSetAllBodiesBelowPhysicsBlendWeight(const FName& InBoneName, float PhysicsBlendWeight, bool bSkipCustomPhysicsType = false, bool bIncludeSelf = true);
+
 };
